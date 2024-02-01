653e36e8769f1a1868d24041e989a1edc1cf155e
mariabellatrice <aureliabelltrc@gmail.com>
2024-02-01 14:29:04 +0700

    push folder migration (utk tabel database)

---
+++ database/migrations/2024_02_01_071840_tabel_koleksi.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('koleksi', function(Blueprint $table) {
+            $table-> id();
+            $table->string('judul_buku', 100);
+            $table->string('pengarang', 100);
+            $table->string('genre', 50);
+            $table->integer("harga");
+            $table->text('gambar');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExist(('koleksi'));
+    }
+};
